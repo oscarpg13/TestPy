bae2f5e977c7bf72b35433660d6413d25b4a5868
oscarpg13 <opg1394@gmail.com>
2023-10-11 14:04:14 +0200

    cambio

--- ejercicio3-7.py
+++ ejercicio3-7.py
@@ -3,7 +3,7 @@ food.insert(0,"pasta")
 food.insert(2,"bollos")
 food.append("fajitas") #append es para añadir un elemento al final 
 food[1] = "lasaña"
-food.remove("tortilla")
+food.pop(-1)
 
 print(food)
 print(food[1])
